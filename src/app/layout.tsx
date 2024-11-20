066dbe83d02282eaaa92ed6b2b4e6808410336ac
ujeans <dbwlsxkal95@gmail.com>
2024-11-21 01:14:30 +0900

    feat(feautre/publish): result 페이지에서는 가운데 정렬 안되게 구현

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { usePathname } from "next/navigation";
 import "./globals.css";
 
 export default function RootLayout({
@@ -5,9 +7,20 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const pathname = usePathname();
+
+  // /result 페이지일 경우 left-align, 아니면 center-align
+  const isResultPage = pathname === "/result";
+
   return (
     <html lang="en">
-      <body className="flex items-center justify-center">
+      <body
+        className={`flex ${
+          isResultPage
+            ? "items-start justify-start"
+            : "items-center justify-center"
+        }`}
+      >
         <div className="max-w-[380px] w-screen h-screen bg-yellow overflow-y-auto scroll-hidden">
           {children}
         </div>
