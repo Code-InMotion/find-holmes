ab416cc452c56a061ff672c66f649424b9874e8f
ujeans <dbwlsxkal95@gmail.com>
2024-11-26 13:33:24 +0900

    fix: https 추가

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -64,7 +64,7 @@ export default function Map({ setMap, lat, lng, zoom }: IMapProps) {
       <Script
         strategy="afterInteractive"
         type="text/javascript"
-        src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_CLIENT}&autoload=false`}
+        src={`https://dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_CLIENT}&autoload=false`}
         onReady={loadKakaoMap}
       />
       <div id="map" className="w-full h-screen"></div>
