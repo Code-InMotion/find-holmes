e9a598ea069039061f1210a64183150598e31cd6
ujeans <dbwlsxkal95@gmail.com>
2024-12-09 21:55:09 +0900

    feat(main): Content-Security-Policy 메타 태그 추가

---
+++ src/app/head.tsx
@@ -0,0 +1,11 @@
+export default function Head() {
+  return (
+    <>
+      <meta
+        http-equiv="Content-Security-Policy"
+        content="upgrade-insecure-requests"
+      />
+      <title>My App</title>
+    </>
+  );
+}
