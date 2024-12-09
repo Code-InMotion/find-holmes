67f173156caf0e9a5841bd3ba2392af9b805c0d6
ujeans <dbwlsxkal95@gmail.com>
2024-12-09 22:18:44 +0900

    feat(main): use client와 metadata를 동시에 쓸수 없는 문제 해결을 위해 app/head.tsx 파일에 저녁 메타 태그 및 SEO 최적화 추가

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -5,7 +5,33 @@ export default function Head() {
         http-equiv="Content-Security-Policy"
         content="upgrade-insecure-requests"
       />
-      <title>My App</title>
+      <meta name="description" content="사용자 맞춤 매물 추천 구해줘, 홈즈!" />
+      <meta name="keywords" content="부동산, 집 구하기, 매물 추천" />
+      <meta property="og:title" content="구해줘, 홈즈" />
+      <meta
+        property="og:description"
+        content="사용자 맞춤 매물 추천 구해줘, 홈즈!"
+      />
+      <meta
+        property="og:url"
+        content={`${process.env.NEXT_PUBLIC_AUTH_REDIRECT_TO_HOME}`}
+      />
+      <meta property="og:site_name" content="구해줘, 홈즈" />
+      <meta
+        property="og:image"
+        content={`${process.env.NEXT_PUBLIC_AUTH_REDIRECT_TO_HOME}/images/system/thumbnail.png`}
+      />
+      <meta property="og:locale" content="ko" />
+      <meta property="og:type" content="website" />
+      <link
+        rel="icon"
+        href={`${process.env.NEXT_PUBLIC_AUTH_REDIRECT_TO_HOME}/images/system/favicon.ico`}
+      />
+      <link
+        rel="apple-touch-icon"
+        href={`${process.env.NEXT_PUBLIC_AUTH_REDIRECT_TO_HOME}/images/system/favicon.png`}
+      />
+      <title>구해줘, 홈즈</title>
     </>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,37 +1,6 @@
 "use client";
 import { usePathname } from "next/navigation";
-import { Metadata } from "next";
 import "./globals.css";
-
-export const metadata: Metadata = {
-  description: "사용자 맞춤 매물 추천 구해줘, 홈즈!",
-  keywords: ["부동산", "집 구하기", "매물 추천"],
-  openGraph: {
-    title: "구해줘, 홈즈",
-    description: "사용자 맞춤 매물 추천 구해줘, 홈즈!",
-    url: `${process.env.NEXT_PUBLIC_AUTH_REDIRECT_TO_HOME}`,
-    siteName: "구해줘, 홈즈",
-    images: [
-      {
-        url: `${process.env.NEXT_PUBLIC_AUTH_REDIRECT_TO_HOME}/images/system/thumbnail.png`,
-        width: 800,
-        height: 600,
-      },
-      {
-        url: `${process.env.NEXT_PUBLIC_AUTH_REDIRECT_TO_HOME}/images/system/thumbnail.png`,
-        width: 1800,
-        height: 1600,
-      },
-    ],
-    locale: "ko",
-    type: "website",
-  },
-  icons: {
-    icon: `${process.env.NEXT_PUBLIC_AUTH_REDIRECT_TO_HOME}/images/system/favicon.ico`,
-    shortcut: `${process.env.NEXT_PUBLIC_AUTH_REDIRECT_TO_HOME}/images/system/favicon.ico`,
-    apple: `${process.env.NEXT_PUBLIC_AUTH_REDIRECT_TO_HOME}/image/systems/favicon.png`,
-  },
-};
 
 export default function RootLayout({
   children,
